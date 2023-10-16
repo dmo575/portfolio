991af61b6369d9c506f5df42d172484f92318916
dmo575 <dmo5@protonmail.com>
2023-10-16 03:12:35 -0700

    adding public dir back

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "./../portfolio_bundle/dist/",
+    outDir: "./../portfolio_backend/dist/",
   },
 })
